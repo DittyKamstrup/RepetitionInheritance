8585a9141c1714d851fee256026f85a96eef1cb6
Ditte <Ditte@DESKTOP-QORFADN>
2017-02-07 11:41:40 +0100

    Property PartikelFilter added to DieselBil

--- RepetitionInheritance/DieselBil.cs
+++ RepetitionInheritance/DieselBil.cs
@@ -8,6 +8,12 @@ namespace RepetitionInheritance
 {
     class DieselBil : Bil
     {
+        public bool PartikelFilter { get; set; }
+
+        // Kig på NedarvningFremlæggelse Repo'et
+
+
+
         public DieselBil(string mærke, int prisExAfgift, int købsÅr, int kmPrLiter) : base(mærke, prisExAfgift, købsÅr, kmPrLiter)
         {
         }
