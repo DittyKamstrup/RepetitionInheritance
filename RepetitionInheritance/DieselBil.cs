a30bf068b1e4c240e9f1bfeec3dd842df72dbc18
Ditte <Ditte@DESKTOP-QORFADN>
2017-02-07 11:35:51 +0100

    class DieselBil added

---
+++ RepetitionInheritance/DieselBil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionInheritance
+{
+    class DieselBil : Bil
+    {
+        public DieselBil(string mærke, int prisExAfgift, int købsÅr, int kmPrLiter) : base(mærke, prisExAfgift, købsÅr, kmPrLiter)
+        {
+        }
+    }
+}
