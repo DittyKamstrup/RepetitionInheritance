297ab06d5f8a1b00a508308c19402aed65630e12
Ditte <Ditte@DESKTOP-QORFADN>
2017-02-07 10:12:51 +0100

    Bil class added with properties

---
+++ RepetitionInheritance/Bil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionInheritance
+{
+    class Bil
+    {
+        public int BilPrisExAfgift { get; set; }
+        public int KøbsÅr { get; set; }
+        public string Mærke { get; set; }
+        public string RegistreringsNr { get; set; }
+        public int KmPrLiter { get; set; }
+
+
+        ////public virtual int RegistreringsAfgift()
+        //{
+            
+        //}
+    }
+}
