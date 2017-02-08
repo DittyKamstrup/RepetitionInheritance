9de8d8bb302f1bc7e8e626a9d102a4eecea05000
Ditte <Ditte@DESKTOP-QORFADN>
2017-02-08 09:44:34 +0100

    Class ElBil added but not done

---
+++ RepetitionInheritance/ElBil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionInheritance
+{
+    public class ElBil : Bil
+    {
+        public ElBil(string mærke, int prisExAfgift, int købsÅr, string registreringsNr, int kmPrLiter, int tank) : base(mærke, prisExAfgift, købsÅr, registreringsNr, kmPrLiter, tank)
+        {
+        }
+
+        public override int HalvÅrligEjerAfgift()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Rækkevidde()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
